7bf5b052e068fba62481fa1230c46808602c57e8
juliossena <juliosouzasena@gmail.com>
2018-07-14 17:13:45 -0300

    testando categoria

--- src/main/java/com/julio/cursomc/resources/CategoriaResources.java
+++ src/main/java/com/julio/cursomc/resources/CategoriaResources.java
@@ -1,15 +1,28 @@
 package com.julio.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.julio.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResources {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST está funcionando!";
+	public List<Categoria> listar() {
+		Categoria cat = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat);
+		lista.add(cat2);
+		
+		return lista;
+		
 	}
 }
