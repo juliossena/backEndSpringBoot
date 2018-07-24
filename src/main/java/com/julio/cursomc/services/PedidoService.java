5d9619aceb21a4412199f073f2fca1334e8fe3eb
juliossena <juliosouzasena@gmail.com>
2018-07-23 23:58:32 -0300

    Implementando to string de pedido

--- src/main/java/com/julio/cursomc/services/PedidoService.java
+++ src/main/java/com/julio/cursomc/services/PedidoService.java
@@ -10,6 +10,7 @@ import com.julio.cursomc.domain.ItemPedido;
 import com.julio.cursomc.domain.PagamentoComBoleto;
 import com.julio.cursomc.domain.Pedido;
 import com.julio.cursomc.domain.enums.EstadoPagamento;
+import com.julio.cursomc.repositories.ClienteRepository;
 import com.julio.cursomc.repositories.ItemPedidoRepository;
 import com.julio.cursomc.repositories.PagamentoRepository;
 import com.julio.cursomc.repositories.PedidoRepository;
@@ -32,6 +33,9 @@ public class PedidoService {
 	
 	@Autowired
 	private ItemPedidoRepository itemPedidoRepository;
+	
+	@Autowired
+	private ClienteService clienteService;
 
 	public Pedido find(Integer id) {
 		Optional<Pedido> obj = repo.findById(id);
@@ -42,6 +46,7 @@ public class PedidoService {
 	public Pedido insert(Pedido obj) {
 		obj.setId(null);
 		obj.setInstante(new Date());
+		obj.setCliente(clienteService.find(obj.getCliente().getId()));
 		obj.getPagamento().setEstado(EstadoPagamento.PENDENTE);
 		obj.getPagamento().setPedido(obj);
 		if (obj.getPagamento() instanceof PagamentoComBoleto) {
@@ -52,10 +57,12 @@ public class PedidoService {
 		pagamentoRepository.save(obj.getPagamento());
 		for (ItemPedido ip : obj.getItens()) {
 			ip.setDesconto(0.0);
-			ip.setPreco(produtoService.find(ip.getProduto().getId()).getPreco());
+			ip.setProduto(produtoService.find(ip.getProduto().getId()));
+			ip.setPreco(ip.getProduto().getPreco());
 			ip.setPedido(obj);
 		}
 		itemPedidoRepository.saveAll(obj.getItens());
+		System.out.println(obj);
 		return obj;
 	}
 }
